760a8101c7bf863f390ef9e0f60444fced4dc798
Tyler C <tyler.christenson@live.com>
2016-11-17 10:10:55 -0700

    Initial Commit
    Initual upload of the game and all it's files.

---
+++ LolSlots/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LolSlots
+{
+    public partial class Form1 : Form
+    {
+        public int coins = 0;
+        public int winnings = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 
+    }
+}
